7d6a7ed69bfe2c1ff3c4d9c0c98179bd25c95f8b
dariok <darioklopic@gmail.com>
2023-08-06 21:54:17 +0200

    Added seek to on click

--- src/renderer/src/components/videoplayer/VideoPlayer.tsx
+++ src/renderer/src/components/videoplayer/VideoPlayer.tsx
@@ -1,16 +1,44 @@
 import { useRef } from 'react'
 import ReactPlayer from 'react-player'
 
-const VideoPlayer = ({ path }): JSX.Element => {
+const VideoPlayer = ({ path, loading, segments }): JSX.Element => {
   const ref = useRef(null)
+
+  const handleSegmentClick = (start, end): void => {
+    if (ref.current) {
+      ;(ref.current as ReactPlayer).seekTo(start, 'seconds')
+    }
+  }
+
   return (
-    <ReactPlayer
-      ref={ref}
-      url={`file-protocol://${path}`}
-      width="100%"
-      height="100%"
-      controls={true}
-    />
+    <div className="flex-col">
+      <ReactPlayer
+        ref={ref}
+        url={`file-protocol://${path}`}
+        width="100%"
+        height="100%"
+        controls={true}
+      />
+      <div className="flex-col h-96 overflow-scroll">
+        {loading ? (
+          <div> Loading... </div>
+        ) : (
+          segments.map(({ text, start, end }) => {
+            return (
+              <div
+                key={start}
+                className="flex-col p-[2px]"
+                onClick={(): void => handleSegmentClick(start, end)}
+              >
+                <div className="flex p-2 bg-green-900/10 h-10 rounded-lg text-green-900 items-center">
+                  <p className="p-4 text-bold">{text}</p>
+                </div>
+              </div>
+            )
+          })
+        )}
+      </div>
+    </div>
   )
 }
 
--- src/renderer/src/pages/Editor.tsx
+++ src/renderer/src/pages/Editor.tsx
@@ -7,28 +7,12 @@ const Editor = (): JSX.Element => {
   const { files, setStep } = useStepContext()
   const { loading, error, response } = useIpc('loadFile', files[0].path)
 
-  // if (loading) return <div>Loading...</div>
   if (error) return <div>There was error processing your file</div>
 
   return (
     <div className="flex-col">
-      <VideoPlayer path={files[0].path} />
+      <VideoPlayer path={files[0].path} loading={loading} segments={response.segments} />
       <div onClick={(): void => setStep(0)}>back</div>
-      <div className="flex-col h-96 overflow-scroll">
-        {loading ? (
-          <div> Loading... </div>
-        ) : (
-          response.segments.map(({ text, start, end }) => {
-            return (
-              <div key={start} className="flex-col p-[2px]">
-                <div className="flex p-2 bg-green-900/10 h-10 rounded-lg text-green-900 items-center">
-                  <p className="p-4 text-bold">{text}</p>
-                </div>
-              </div>
-            )
-          })
-        )}
-      </div>
     </div>
   )
 }
